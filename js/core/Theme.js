51ca4b02a71f81908096036d6c06479624b8262c
Benoit Gauthier <bgauthier@metadocx.com>
2023-01-02 04:40:59 +0000

    Default theme

--- js/core/Theme.js
+++ js/core/Theme.js
@@ -9,11 +9,56 @@ class Theme {
 
     constructor(app) {
         this.app = app;
+        this.colorScheme = ['#9999ff', '#993366', '#ffffcc', '#ccffff', '#660066', '#ff8080', '#0066cc', '#ccccff', '#000080', '#ff00ff', '#ffff00', '#0000ff', '#800080', '#800000', '#008080', '#0000ff'];
+        this.applyChartColorTheme();
     }
 
-    renderCoverPage() { return ''; }
-    renderCoverPageCSS() { return ''; }
+    renderCoverPage() {
+        var s = '';
+
+        s += `<div class="report-cover-page">
+            <div class="report-cover-header"></div>
+            <div class="report-cover-name">${this.app.viewer.report.getReportDefinition().properties.name}</div>
+            <div class="report-cover-description">${this.app.viewer.report.getReportDefinition().properties.description}</div>
+            <div class="report-cover-footer"></div>
+        </div>`;
+
+        return s;
+    }
+    renderCoverPageCSS() {
+        return `
+
+            #reportCoverPage {
+                position:relative;
+            }
+
+            .report-cover-page {
+                height: 100%;
+            }
+
+            .report-cover-name {
+                position: absolute;
+                top: 360px;
+                font-size: 36px;
+                font-weight: bold;
+            }
+
+            .report-cover-description {
+                position: absolute;
+                top: 410px;                
+            }
+        `;
+    }
+
     renderThemeCSS() { return ''; }
+
+    getColorScheme() {
+        return this.colorScheme;
+    }
+
+    applyChartColorTheme() {
+
+    }
 
 }
 
