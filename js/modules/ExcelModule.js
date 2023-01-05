fb9ec9852255cf7aef6737695aa500684c3b9a6c
Benoit Gauthier <bgauthier@metadocx.com>
2023-01-05 18:03:33 +0000

    Add export to excel, bypass options for now

--- js/modules/ExcelModule.js
+++ js/modules/ExcelModule.js
@@ -20,6 +20,8 @@ class ExcelModule extends Module {
 
     showExportDialog() {
 
+        return this.exportExcel();
+
         if (this.exportDialog === null) {
             $(this.app.viewer.getContainerSelector()).append(this.renderExportDialog());
             this.hookExportDialogComponents();
@@ -250,6 +252,8 @@ class ExcelModule extends Module {
 
     getExcelExportOptions() {
 
+        return {};
+
         var orientation = Metadocx.modules.Printing.PageOrientation.Portrait;
         if ($('#excelOrientationLandscape').prop('checked')) {
             orientation = Metadocx.modules.Printing.PageOrientation.Landscape;
@@ -288,26 +292,46 @@ class ExcelModule extends Module {
 
     }
 
-    exportexcel() {
+    exportExcel() {
+
+        /**
+         *  Get all graphs images and encode them in base64 to insert in excel
+         */
+        var thisObject = this;
+
+        /**
+         * Show exporting dialog
+         */
+        var exportDialog = bootbox.dialog({
+            title: 'Export to Excel',
+            message: '<p><i class="fas fa-spin fa-spinner"></i> Exporting report to Excel...</p>'
+        });
+
+
         $.ajax({
             type: 'post',
-            url: '/Convert/excel',
+            url: '/Metadocx/Convert/Excel',
             data: {
                 ExportOptions: this.getExcelExportOptions(),
-                HTML: btoa(unescape(encodeURIComponent($('#reportPage').html()))),
+                ReportDefinition: JSON.parse(JSON.stringify(this.app.viewer.report.getReportDefinition())),
+                Graphs: [],
             },
             xhrFields: {
                 responseType: 'blob'
             },
             success: (data, status, xhr) => {
-                //console.log(data);
-                //console.log(status);
-
                 var blob = new Blob([data]);
-                var link = document.createElement('a');
-                link.href = window.URL.createObjectURL(blob);
-                link.download = "Report.xls";
-                link.click();
+
+                var sContent = `Report has been converted to Excel, click on button to download file<br><br>
+                <a class="btn btn-primary" href="${window.URL.createObjectURL(blob)}" download="Report.xlsx" onClick="$('.bootbox.modal').modal('hide');">Download report</a>`;
+
+                exportDialog.find('.bootbox-body').html(sContent);
+
+                thisObject.app.modules.Printing.applyPageStyles();
+
+                $('.report-graph-canvas').show();
+                $('.report-graph-image').hide();
+
 
             }
         });
