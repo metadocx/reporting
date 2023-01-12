d258acf352207cc6badca440ccf74be31e3a1cdd
Benoit Gauthier <bgauthier@metadocx.com>
2023-01-12 05:36:36 +0000

    #59 Criteria tests for date and select

---
+++ test/tests/DateTimeCriteriaTest.js
@@ -0,0 +1,50 @@
+const assert = require('chai').assert
+const expect = require('chai').expect;
+const addContext = require('mochawesome/addContext');
+
+/**
+ * Cover page tests
+ */
+describe('DateTime Criteria', function () {
+
+    let page;
+
+    before(async function () {
+        page = await browser.newPage();
+        await page.goto('http://localhost:8080', {
+            waitUntil: "networkidle0"
+        });
+    });
+
+    after(async function () {
+        await page.close();
+    });
+
+    it('Filter timesheets for date period', async function () {
+
+        /**
+         * Enable metadocx cover page and refresh report
+         */
+        let numberOfRows = await page.evaluate(() => {
+            Metadocx.viewer.options.coverPage.enabled = false;
+            Metadocx.viewer.getCriteria('TimesheetDate').setIsEnabled(true)
+            Metadocx.viewer.getCriteria('TimesheetDate').setValue({
+                startDate: '2022-12-01',
+                endDate: '2022-12-31'
+            });
+            Metadocx.viewer.refreshReport();
+
+            return $('.report-row-data').length;
+
+        });
+
+        await page.screenshot({ path: './test/bootstrap/images/filter-dates.png', fullPage: true });
+
+        addContext(this, '/images/filter-dates.png');
+
+        expect(numberOfRows).to.be.equal(6);
+
+    });
+
+
+});
